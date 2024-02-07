8fd1a5729b74432882c56b2d22e0072dfc93da70
Diego <diegolima.almeida93@gmail.com>
2024-02-07 13:37:21 -0300

    feat: games section, card game component #11

---
+++ src/components/cards/gameCard.tsx
@@ -0,0 +1,21 @@
+import { ReactNode } from "react";
+
+interface GameCardProps {
+  icon: ReactNode;
+  title: string;
+  description: string;
+}
+
+export const GameCard = ({ icon, title, description }: GameCardProps) => {
+  return (
+    <div className="flex h-44 w-full items-center justify-evenly rounded-md border border-lime6 bg-gradient-to-b from-lime3 to-lime5">
+      <div className="text-8xl">{icon}</div>
+      <div className="flex w-3/5 flex-col">
+        <div>
+          <h2 className="text-center text-2xl font-medium">{title}</h2>
+          <p className="text-center text-lime11">{description}</p>
+        </div>
+      </div>
+    </div>
+  );
+};
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,16 @@
 import { Button } from "@/components/button/Button";
 
-import { InfoCard } from "@/components/info-card/InfoCard";
+import { InfoCard } from "@/components/cards/InfoCard";
+import { GameCard } from "@/components/cards/gameCard";
 
 import { FaClock } from "react-icons/fa";
 import { BsFillPeopleFill } from "react-icons/bs";
 import { FaBook } from "react-icons/fa";
+import { FiTarget } from "react-icons/fi";
 
 export default function Home() {
   return (
-    <main>
+    <main className="flex flex-col gap-6">
       <section className="flex min-h-screen items-center bg-lime3">
         <div className="container-width flex flex-col items-center gap-5">
           <h1 className="text-4xl font-semibold">Human Benchmark</h1>
@@ -37,6 +39,23 @@ export default function Home() {
             description="Lorem Ipsum is simply dummy text of the printing."
           />
         </div>
+      </section>
+      <section className="container-width gap6 flex min-h-screen flex-col gap-4">
+        <GameCard
+          icon={<FiTarget />}
+          title="Titulo do jogo"
+          description="Descrição do jogo"
+        />
+        <GameCard
+          icon={<FiTarget />}
+          title="Titulo do jogo"
+          description="Descrição do jogo"
+        />
+        <GameCard
+          icon={<FiTarget />}
+          title="Titulo do jogo"
+          description="Descrição do jogo"
+        />
       </section>
     </main>
   );
