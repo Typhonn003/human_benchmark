25ca70ec193197b0dfdc875c10f3edd339e6fbac
Diego <diegolima.almeida93@gmail.com>
2024-02-08 17:30:56 -0300

    refactor: game card width

--- src/components/cards/gameCard.tsx
+++ src/components/cards/gameCard.tsx
@@ -8,9 +8,9 @@ interface GameCardProps {
 
 export const GameCard = ({ icon, title, description }: GameCardProps) => {
   return (
-    <div className="group flex h-44 w-full items-center justify-evenly rounded-md border border-lime6 bg-gradient-to-b from-lime3 to-lime5 shadow-md hover:border-lime8 transition transform hover:-translate-y-2 motion-reduce:transition-none motion-reduce:hover:transform-none">
+    <div className="card-animation group flex h-44 w-full items-center justify-evenly rounded-md border border-lime6 bg-gradient-to-b from-lime3 to-lime5 shadow-md hover:border-lime8">
       <div className="text-8xl group-hover:text-lime11">{icon}</div>
-      <div className="smartphone:w-3/6 flex w-3/5 flex-col">
+      <div className="flex w-3/5 flex-col">
         <div>
           <h2 className="text-center text-2xl font-medium">{title}</h2>
           <p className="text-center text-lime11">{description}</p>
