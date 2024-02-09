570e9845db01d17d5aace4626f5b3fcdb1bc6ccb
Diego <diegolima.almeida93@gmail.com>
2024-02-09 12:40:49 -0300

    feat: home page background balls #11

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -73,6 +73,7 @@ const config = {
       animation: {
         "accordion-down": "accordion-down 0.2s ease-out",
         "accordion-up": "accordion-up 0.2s ease-out",
+        "spin-slow": "spin 300s linear infinite",
       },
       screens: {
         smartphone: "375px",
