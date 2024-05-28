3deddecbe8b59a14e206b66816859e3bac2cefe3
gpmlad <giovanniperotto@hotmail.com>
2024-05-27 22:44:10 -0300

    feat: add login and cookies

--- src/pages/profile.tsx
+++ src/pages/profile.tsx
@@ -1,7 +1,68 @@
+import { useFetch } from "@/hooks/useFetch";
+import { useRouter } from "next/router";
+import { parseCookies } from "nookies";
+
+export interface IUserScore {
+  id: string;
+  name: string;
+  username: string;
+  email: string;
+  role: string;
+  active: boolean;
+  user_points: UserPoint[];
+}
+
+export interface UserPoint {
+  id: string;
+  user_id: string;
+  game_id: string;
+  points: number;
+  game: Game;
+}
+
+export interface Game {
+  id: string;
+  name: string;
+}
+
 export default function Profile() {
+  const router = useRouter();
+  const cookies = parseCookies();
+  const token = cookies["h-benchmark"];
+  console.log(token);
+  const { data, isLoading, mutate } = useFetch<IUserScore>(
+    "users/profile",
+    token,
+  );
+
+  if (isLoading) return <div>Buscando dados...</div>;
+
+  if (!data) {
+    router.push("/");
+    return;
+  }
+
+  /* return (
+    <ul>
+      {data.user_points.map(({ points, id }) => {
+        return <li key={id}>{points}</li>;
+      })}
+    </ul>
+  ); */
+
   return (
-    <>
-      <h2>Profile</h2>
-    </>
+    <main className="mt-10">
+      <h1>Olá {data.name}</h1>
+      <span>Sua pontuação:</span>
+      <ul>
+        {data.user_points.map(({ points, id, game }) => {
+          return (
+            <li key={id}>
+              {points} ponto no game {game.name}
+            </li>
+          );
+        })}
+      </ul>
+    </main>
   );
 }
