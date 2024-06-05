78ce801a6cef8dbcb40624e9135682a112a434f3
Diego <diegolima.almeida93@gmail.com>
2024-06-05 15:41:21 -0300

    feat: add old components with new name

---
+++ src/components/cards/InfoCard.tsx
@@ -0,0 +1,25 @@
+import { ReactNode } from "react";
+
+interface InfoCardProps {
+  icon: ReactNode;
+  title: string;
+  description: string;
+}
+
+const InfoCard = ({ icon, title, description }: InfoCardProps) => {
+  return (
+    <div className="flex w-full max-w-96 items-center justify-between gap-4">
+      <div className="flex min-h-24 min-w-24 items-center justify-center rounded-md bg-lime9 text-6xl">
+        {icon}
+      </div>
+      <div>
+        <h2 className="text-xl font-medium laptop:text-2xl">{title}</h2>
+        <p className="text-justify text-base text-lime11 desktop:text-lg">
+          {description}
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export { InfoCard };
