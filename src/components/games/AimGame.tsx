457bd21934cb639c0760f3980f322f7059ef8a3f
gpmlad <giovanniperotto@hotmail.com>
2024-06-06 21:43:36 -0300

    feat: config aim game to use zustand and change aim image and sound effect

--- src/components/games/AimGame.tsx
+++ src/components/games/AimGame.tsx
@@ -1,4 +1,4 @@
-import { useUserStore } from "@/store";
+import { useGameStatusStore, useUserStore } from "@/store";
 import { useRef, useEffect } from "react";
 
 interface Iposition {
@@ -10,6 +10,12 @@ class Circle {
   position: Iposition;
   radius: number;
   ctx: CanvasRenderingContext2D;
+  image: HTMLImageElement;
+  old: number;
+  now: number;
+  score: number;
+  maxScore: number;
+
   constructor(
     x: number,
     y: number,
@@ -19,14 +25,41 @@ class Circle {
     this.position = { x: x, y: y };
     this.radius = radius;
     this.ctx = ctx;
+    this.image = new Image(radius * 2, radius * 2);
+    this.old = 0;
+    this.now = 0;
+    this.score = 0;
+    this.maxScore = 30;
   }
 
   draw() {
-    this.ctx.fillStyle = "red";
+    this.ctx.lineWidth = 1;
+    this.ctx.strokeStyle = "red";
+
     this.ctx.beginPath();
-    this.ctx.arc(this.position.x, this.position.y, this.radius, 0, Math.PI * 2);
+    this.ctx.arc(
+      this.position.x,
+      this.position.y,
+      this.radius,
+      0,
+      2 * Math.PI,
+      false,
+    );
     this.ctx.stroke();
-    this.ctx.fill();
+    this.ctx.closePath();
+
+    // Desenhar a linha horizontal
+    this.ctx.beginPath();
+    this.ctx.moveTo(this.position.x - this.radius, this.position.y);
+    this.ctx.lineTo(this.position.x + this.radius, this.position.y);
+    this.ctx.stroke();
+    this.ctx.closePath();
+
+    // Desenhar a linha vertical
+    this.ctx.beginPath();
+    this.ctx.moveTo(this.position.x, this.position.y - this.radius);
+    this.ctx.lineTo(this.position.x, this.position.y + this.radius);
+    this.ctx.stroke();
     this.ctx.closePath();
   }
 
@@ -49,16 +82,46 @@ class Circle {
       return false;
     }
   }
+
+  drawTime(ctx: CanvasRenderingContext2D) {
+    this.now = Date.now();
+    ctx.fillStyle = "#37401C";
+    ctx.font = "24px serif";
+    const textContent = `Tempo: ${((this.now - this.old) / 1000).toFixed(1)}`;
+    const textSize = Math.floor(
+      ctx.measureText(textContent).actualBoundingBoxDescent,
+    );
+    ctx.fillText(textContent, 10, ctx.canvas.height - textSize - 10);
+  }
+
+  drawScore(ctx: CanvasRenderingContext2D, canvas: HTMLCanvasElement) {
+    ctx.fillStyle = "#37401C";
+    ctx.font = "36px serif";
+    const textContent = `${this.score}`;
+    const textSize = Math.floor(ctx.measureText(textContent).width);
+    ctx.fillText(textContent, canvas.width - textSize - 10, 52);
+  }
+
+  update(srcAim: string) {
+    this.image.src = srcAim;
+  }
+
+  reset() {
+    this.old = 0;
+    this.now = 0;
+    this.score = 0;
+  }
 }
 
 const AimGame = () => {
-  const { user } = useUserStore();
+  const { setGameFinished, setGameScore, setFinalScreen } =
+    useGameStatusStore();
   const canvasRef = useRef<HTMLCanvasElement | null>(null);
-  let score = 0;
   const circle = useRef<Circle | null>(null);
 
   const cleanScreen = (context: CanvasRenderingContext2D) => {
-    context.clearRect(0, 0, context.canvas.width, context.canvas.height);
+    context.fillStyle = "#bdee63";
+    context.fillRect(0, 0, context.canvas.width, context.canvas.height);
   };
 
   const animate = () => {
@@ -70,9 +133,14 @@ const AimGame = () => {
 
         if (!circle.current) {
           circle.current = new Circle(50, 50, 20, context);
+          circle.current.update("/shot.png");
         }
 
         circle.current.draw();
+        if (circle.current.old) {
+          circle.current.drawTime(context);
+          circle.current.drawScore(context, canvas);
+        }
       }
       requestAnimationFrame(animate);
     }
@@ -89,8 +157,19 @@ const AimGame = () => {
             (e as MouseEvent).offsetY,
           )
         ) {
+          if (!circle.current.old) {
+            circle.current.old = Date.now();
+          }
           circle.current.move();
-          score += 1;
+          if (circle.current.score == circle.current.maxScore - 1) {
+            const finalScore = parseFloat(
+              ((circle.current.now - circle.current.old) / 1000).toFixed(2),
+            );
+            setGameScore(finalScore);
+            setGameFinished(true);
+            setFinalScreen(true);
+          }
+          circle.current.score += 1;
           const AudioClass =
             typeof window !== "undefined" ? window.Audio : null;
 
@@ -125,7 +204,9 @@ const AimGame = () => {
     };
   }, []);
 
-  return <canvas className="border-2 border-white" ref={canvasRef} />;
+  return (
+    <canvas ref={canvasRef} width={500} height={500} className="rounded-md" />
+  );
 };
 
 export { AimGame };
