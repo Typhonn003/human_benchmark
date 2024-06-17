a6e6c6536b12f281fc0cc43d5a084a26ec15ebe4
Diego <diegolima.almeida93@gmail.com>
2024-06-17 13:14:43 -0300

    feat: change api base url

--- src/services/axios.ts
+++ src/services/axios.ts
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:3001",
-  timeout: 3000,
+  baseURL: "https://h-bench-api.sambacore.com/",
+  timeout: 5000,
 });
 
 export default api;
