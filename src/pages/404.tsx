cf988eb813c5e31364b5b0043a6ffbfe9161c262
Diego <diegolima.almeida93@gmail.com>
2024-06-18 17:50:35 -0300

    feat: metatags component, all pages metatags

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -2,7 +2,7 @@ import { useRouter } from "next/router";
 import { useUserStore } from "@/store";
 import { useEffect } from "react";
 
-import { Button } from "@/components";
+import { Button, MetaTags } from "@/components";
 import { inter, poppins } from "@/fonts";
 
 const Custom404Page = () => {
@@ -15,22 +15,27 @@ const Custom404Page = () => {
   }, [fetch]);
 
   return (
-    <main
-      className={`${inter.className} screen-height-without-header container-width flex items-center justify-center`}
-    >
-      <div className="flex max-w-96 flex-col gap-4">
-        <h2 className={`${poppins.className} text-center text-4xl font-medium`}>
-          Que pena...
-        </h2>
-        <h3 className="text-center text-lg text-lime11">
-          Parece que não encontramos o que você estava procurando.{" "}
-          <span className="font-semibold">=(</span>
-        </h3>
-        <Button onClick={() => router.push(redirectPath)}>
-          {user ? "Ir para perfil" : "Ir para tela inicial"}
-        </Button>
-      </div>
-    </main>
+    <>
+      <MetaTags pageName="404 - Não Encontrado" />
+      <main
+        className={`${inter.className} screen-height-without-header container-width flex items-center justify-center`}
+      >
+        <div className="flex max-w-96 flex-col gap-4">
+          <h2
+            className={`${poppins.className} text-center text-4xl font-medium`}
+          >
+            Que pena...
+          </h2>
+          <h3 className="text-center text-lg text-lime11">
+            Parece que não encontramos o que você estava procurando.{" "}
+            <span className="font-semibold">=(</span>
+          </h3>
+          <Button onClick={() => router.push(redirectPath)}>
+            {user ? "Ir para perfil" : "Ir para tela inicial"}
+          </Button>
+        </div>
+      </main>
+    </>
   );
 };
 
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -11,6 +11,7 @@ import {
   CardTitle,
   GameCard,
   LoginForm,
+  MetaTags,
   RegisterForm,
   gamesInfo,
 } from "@/components";
@@ -43,64 +44,67 @@ const Home = () => {
   }
 
   return (
-    <main className={`${inter.className} flex flex-col`}>
-      <section className="screen-height-without-header flex items-center bg-lime3 tablet:h-auto tablet:py-10">
-        <div className="container-width desktop:gap-38 flex max-w-96 flex-col items-center gap-5 tablet:max-w-full tablet:flex-row ">
-          <div className="relative flex w-full flex-col items-center gap-4 tablet:w-2/4 laptop:items-start laptop:gap-8 desktop:w-3/5">
-            <h1
-              className={`${poppins.className} font- z-10 text-center text-3xl font-medium smartphone:text-[2.3rem] tablet:text-[2rem] laptop:text-5xl  desktop:text-6xl`}
-            >
-              Human Benchmark
-            </h1>
-            <div
-              className={`${poppins.className} z-10 w-72 text-xl text-lime11 smartphone:block laptop:w-full laptop:text-3xl desktop:text-4xl`}
-            >
-              <p>Se desafie.</p>
-              <p className="text-center">Aprimore suas habilidades.</p>
-              <p className="text-right">Supere seus limites.</p>
+    <>
+      <MetaTags pageName="Human Benchmark" />
+      <main className={`${inter.className} flex flex-col`}>
+        <section className="screen-height-without-header flex items-center bg-lime3 tablet:h-auto tablet:py-10">
+          <div className="container-width desktop:gap-38 flex max-w-96 flex-col items-center gap-5 tablet:max-w-full tablet:flex-row ">
+            <div className="relative flex w-full flex-col items-center gap-4 tablet:w-2/4 laptop:items-start laptop:gap-8 desktop:w-3/5">
+              <h1
+                className={`${poppins.className} font- z-10 text-center text-3xl font-medium smartphone:text-[2.3rem] tablet:text-[2rem] laptop:text-5xl  desktop:text-6xl`}
+              >
+                Human Benchmark
+              </h1>
+              <div
+                className={`${poppins.className} z-10 w-72 text-xl text-lime11 smartphone:block laptop:w-full laptop:text-3xl desktop:text-4xl`}
+              >
+                <p>Se desafie.</p>
+                <p className="text-center">Aprimore suas habilidades.</p>
+                <p className="text-right">Supere seus limites.</p>
+              </div>
+              <div className="z-10 flex w-full flex-col justify-between gap-2 smartphone:flex-row smartphone:justify-start tablet:flex-col  laptop:flex-row">
+                <Button size="lg" onClick={goPlay}>
+                  Jogo aleatório
+                </Button>
+                {
+                  //<Button size="lg" variant="outline">Ver resultados detalhados</Button>
+                }
+              </div>
+              <div className="absolute -top-4 h-48 w-48 animate-spin-slow rounded-full bg-lime4 smartphone:-top-6 smartphone:h-56 smartphone:w-56 tablet:h-64 tablet:w-64 laptop:-top-4 laptop:left-20 laptop:h-72 laptop:w-72 desktop:-top-4 desktop:left-28 desktop:h-80 desktop:w-80">
+                <div className="absolute -bottom-5 -right-5 h-24 w-24 rounded-full bg-lime5 smartphone:h-32 smartphone:w-32 laptop:h-40 laptop:w-40 desktop:h-48 desktop:w-48" />
+              </div>
             </div>
-            <div className="z-10 flex w-full flex-col justify-between gap-2 smartphone:flex-row smartphone:justify-start tablet:flex-col  laptop:flex-row">
-              <Button size="lg" onClick={goPlay}>
-                Jogo aleatório
-              </Button>
-              {
-                //<Button size="lg" variant="outline">Ver resultados detalhados</Button>
-              }
-            </div>
-            <div className="absolute -top-4 h-48 w-48 animate-spin-slow rounded-full bg-lime4 smartphone:-top-6 smartphone:h-56 smartphone:w-56 tablet:h-64 tablet:w-64 laptop:-top-4 laptop:left-20 laptop:h-72 laptop:w-72 desktop:-top-4 desktop:left-28 desktop:h-80 desktop:w-80">
-              <div className="absolute -bottom-5 -right-5 h-24 w-24 rounded-full bg-lime5 smartphone:h-32 smartphone:w-32 laptop:h-40 laptop:w-40 desktop:h-48 desktop:w-48" />
-            </div>
+            <Card className="z-10 flex w-full flex-col bg-lime2 tablet:w-2/4 desktop:w-2/5">
+              <CardHeader>
+                <CardTitle className={`${poppins.className} font-medium`}>
+                  Entrar
+                </CardTitle>
+              </CardHeader>
+              <CardContent>
+                <LoginForm />
+              </CardContent>
+              <div className="w-full border-b-[1px]" />
+              <CardFooter className="p-6">
+                <RegisterForm />
+              </CardFooter>
+            </Card>
           </div>
-          <Card className="z-10 flex w-full flex-col bg-lime2 tablet:w-2/4 desktop:w-2/5">
-            <CardHeader>
-              <CardTitle className={`${poppins.className} font-medium`}>
-                Entrar
-              </CardTitle>
-            </CardHeader>
-            <CardContent>
-              <LoginForm />
-            </CardContent>
-            <div className="w-full border-b-[1px]" />
-            <CardFooter className="p-6">
-              <RegisterForm />
-            </CardFooter>
-          </Card>
-        </div>
-      </section>
-      <section className="py-8">
-        <ul className="container-width flex flex-col gap-4 tablet:grid tablet:grid-cols-2 laptop:grid-cols-3">
-          {gamesInfo.map(({ icon, name, description, title }) => (
-            <GameCard
-              key={name}
-              icon={icon}
-              title={title}
-              name={name}
-              description={description}
-            />
-          ))}
-        </ul>
-      </section>
-    </main>
+        </section>
+        <section className="py-8">
+          <ul className="container-width flex flex-col gap-4 tablet:grid tablet:grid-cols-2 laptop:grid-cols-3">
+            {gamesInfo.map(({ icon, name, description, title }) => (
+              <GameCard
+                key={name}
+                icon={icon}
+                title={title}
+                name={name}
+                description={description}
+              />
+            ))}
+          </ul>
+        </section>
+      </main>
+    </>
   );
 };
 
