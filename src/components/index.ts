ba50a671e68b28948c2ac8da97970b1ea1ef3650
Diego <diegolima.almeida93@gmail.com>
2024-06-18 13:48:37 -0300

    feat: use is mobile hook, update button, change header name

--- src/components/index.ts
+++ src/components/index.ts
@@ -11,7 +11,6 @@ export * from "./modal/EditProfile";
 export * from "./ui/Button";
 export * from "./ui/Card";
 export * from "./ui/Dialog";
-export * from "./ui/DropdownMenu";
 export * from "./ui/Form";
 export * from "./ui/Input";
 export * from "./ui/Label";
--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -5,10 +5,13 @@ import { useRouter } from "next/router";
 
 import { Button } from "@/components";
 import { inter, poppins } from "@/fonts";
+import useIsMobile from "@/hooks/useIsMobile";
 
 const Header = () => {
   const { user, destroyUser } = useUserStore();
   const router = useRouter();
+  const mobile = useIsMobile();
+
   const handleLogout = () => {
     destroyUser();
     destroyCookie(null, "h-benchmark");
@@ -20,27 +23,27 @@ const Header = () => {
       <div className="container-width mx-auto flex items-center justify-between py-2">
         <Link href="/">
           <h1 className={`${poppins.className} text-lg font-medium`}>
-            Human Benchmark
+            {mobile ? "H Benchmark" : "Human Benchmark"}
           </h1>
         </Link>
-        {user ? (
+        {user && (
           <ul className="flex gap-2">
             <li>
               <Button
                 variant="outline"
-                className="h-7"
+                size="sm"
                 onClick={() => router.push("/profile")}
               >
                 Perfil
               </Button>
             </li>
             <li>
-              <Button variant="outline" className="h-7" onClick={handleLogout}>
+              <Button variant="outline" size="sm" onClick={handleLogout}>
                 Sair
               </Button>
             </li>
           </ul>
-        ) : null}
+        )}
       </div>
     </header>
   );
